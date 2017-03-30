9aa4509ab2a335c15353f66efec5548a3b9fdaa3
Alejandro Isla <willywes_69@hotmail.com>
2017-03-30 17:39:28 -0300

    Para la Esthefi

---
+++ Source/Solucion_Clinica/AppWebService/App_Code/ClinicaWebService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "ClinicaWebService" en el código, en svc y en el archivo de configuración a la vez.
+public class ClinicaWebService : IClinicaWebService
+{
+    public void DoWork()
+    {
+    }
+}
